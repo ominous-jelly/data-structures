90734cc0d9f98b604436965d56eabef35f82eb31
Christopher Jenkins <topher.r.jenkins@gmail.com>
2023-06-11 12:52:33 -0700

    Update to linked_lists

---
+++ docs/README.md
@@ -0,0 +1,15 @@
+# Python Data Structures
+
+This is simply for me to help keep track of what I'm learning as I progress in my Python skills.
+
+There's nothing usable here, but there are some decent notes, if that's what you're into.
+
+Feel free to use/clone/alter/ignore/suggest. That's what the repo is public for after all. 
+
+## Completed scripts
+
+1. Nodes
+
+## In progress scripts
+
+1. Linked Lists
--- src/linked_lists.py
+++ src/linked_lists.py
@@ -30,7 +30,7 @@ class Node():
 class LinkedList():
 
 # When creating a list, the only thing we need to pass it is a value for the head node
-    def __init__(self, value=None):
+    def __init__(self, value):
         self.head_node = Node(value)
 
 # Method to return the head node of the linked list
@@ -54,8 +54,61 @@ class LinkedList():
             current_node = current_node.get_next_node()                     # 5. Moves on to the next node in line (if there is one)
         return string_list                                                  # 6. Returns our cobbled together string.
 
-ll = LinkedList(5)
-ll.insert_beginning(24)
-ll.insert_beginning(3)
+# Method to remove a value from the linked list.
+    def remove_value(self, value_to_remove):                                # 1. This will take a single value to iterate (or traverse) the list for
+        current_node = self.get_head_node()                                 # 2. Sets a "current node" so we know where we're at
+        if current_node.get_value() == value_to_remove:                     # 3. Goes ahead and checks the first item in the list first, just in case.
+            self.head_node = current_node.get_next_node()                   # 4. If it is the first item, just sets the head node as the next node.
+        else:                                                               #
+            while current_node:                                             # 5. Otherwise, let's do this for every item in the linked list
+                next_node = current_node.get_next_node()                    # 6. Sets a variable that we can use to look at the next node
+                if next_node.get_value() == value_to_remove:                # 7. Checks to see if the next node's value is what we're looking for.
+                    current_node.set_next_node(next_node.get_next_node())   # 8. If it is, let's set the current node's link to the node after the removed one.
+                    current_node = None                                     # 9. Set current node to None (so we can break out of the while loop)
+                else:                                                       #
+                    current_node = next_node                                # 10. Otherwise, move on to the next
 
+# Method for removing ALL instances of a value from the linked list.
+    def remove_all_values(self, value_to_remove):                           
+        current_node = self.get_head_node()
+        while True:
+            if current_node.get_value() == value_to_remove:                 
+                self.head_node = current_node.get_next_node()                   
+                current_node = self.get_head_node()
+            else:
+                break
+        while current_node:
+            next_node = current_node.get_next_node()
+            if next_node == None:
+                return None
+            else:
+                if next_node.get_value() == value_to_remove:
+                    current_node.set_next_node(next_node.get_next_node())
+                else:
+                    current_node = next_node
+
+
+
+# Method for returning the node with a certain value (searching the linked list)
+    def find_value(self, search_value):
+        current_node = self.get_head_node()                                 # 1. Sets the current node
+        search_results = []                                                 # 2. Creates an empty list to store the results
+        while current_node:                                                 # 
+            if current_node.get_value() == search_value:                    # 3. If the current node is equal to the value we're searching for...
+                search_results.append(current_node)                         # 4. Pop that hoe in the search results list
+            current_node = current_node.get_next_node()                     # 5. Move on to the next, until that shit is worn down.
+        return search_results                                               # 6. Return it.
+
+
+
+
+ll = LinkedList(10)
+ll.insert_beginning(20)
+ll.insert_beginning(30)
+ll.insert_beginning(10)
+ll.insert_beginning(20)
+ll.insert_beginning(10)
+
+ll.remove_all_values(10)
 print(ll.stringify_list())
+
